b2fa69d568f4be289800559b6908ef50681f8f1b
Andrew Ault <andrew.ault@outlook.com>
2020-01-31 23:50:09 -0500

    add https

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
+
+//
+const app = express();
 
 const root = path.join(__dirname, 'docs');
-
-const app = express();
 app.use(express.static(root));
 
+//
 const index = path.join(root, 'index.html');
 app.get('*', (req, res) => res.sendFile(index));
 
+//
+const httpsOptions = {
+  cert: fs.readFileSync(path.join(__dirname, 'ssl/andrewault.me.crt')),
+  ca: fs.readFileSync(path.join(__dirname, 'ssl/andrewault.me.ca-bundle')),
+  key: fs.readFileSync(path.join(__dirname, 'ssl/andrewault.me.key'))
+};
+
+const server = https.createServer(httpsOptions, app);
 const port = process.env.PORT || 80;
-app.listen(port, () => console.log('running on ' + port));
+server.listen(port, () => console.log('running on port ' + port));
