7da0dbf43480b08a63648d20e8e445f6efb28745
andyault <andrew.ault@outlook.com>
2024-09-22 17:17:10 -0400

    add dotenv, add option for https

--- scripts/serve.js
+++ scripts/serve.js
@@ -4,26 +4,26 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
+// config
+require('dotenv').config();
+
+const useHttps = process.env.PORTFOLIO_USE_HTTPS;
 const certPath = process.env.PORTFOLIO_CERT_PATH;
 const caPath = process.env.PORTFOLIO_CA_PATH;
 const keyPath = process.env.PORTFOLIO_KEY_PATH;
-
-const httpsOptions = {
-  cert: fs.readFileSync(certPath),
-  ca: caPath ? fs.readFileSync(caPath) : undefined,
-  key: fs.readFileSync(keyPath),
-};
 
 //
 const app = express();
 
 // redirect to https
-app.use((req, res, next) => {
-  if (req.protocol === 'http') {
-    res.redirect(301, `https://${req.headers.host}${req.url}`);
-  }
-  next();
-});
+if (useHttps) {
+  app.use((req, res, next) => {
+    if (req.protocol === 'http') {
+      res.redirect(301, `https://${req.headers.host}${req.url}`);
+    }
+    next();
+  });
+}
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
@@ -31,9 +31,17 @@ app.use(express.static(path.join(__dirname, '../dist')));
 const index = path.join(__dirname, '../dist/index.html');
 app.get('*', (_, res) => res.sendFile(index));
 
-// serve both http and https
+// run server
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(httpsOptions, app);
-
 httpServer.listen(80);
-httpsServer.listen(443);
+
+if (useHttps) {
+  const httpsOptions = {
+    cert: fs.readFileSync(certPath),
+    ca: caPath ? fs.readFileSync(caPath) : undefined,
+    key: fs.readFileSync(keyPath),
+  };
+
+  const httpsServer = https.createServer(httpsOptions, app);
+  httpsServer.listen(443);
+}
